43a2e2ffa118b211cc1f52171eb7602ce3121ffb
Aanal Patil <123722815+aanalpatil24@users.noreply.github.com>
2026-04-14 03:50:17 +0530

    remove framework from poc branch

--- HGNN_Trajectory_Prediction_Framework/src/training/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Training and evaluation."""
-from .trainer import Trainer
-from .loss import TrajectoryLoss
-from .metrics import compute_ade_fde, compute_collision_rate
--- HGNN_Trajectory_Prediction_Framework/src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility functions."""
-from .config import ModelConfig
-from .hypergraph_builder import HypergraphConstructor
-from .logger import setup_logger
-from .visualization import plot_trajectories, plot_hypergraph
